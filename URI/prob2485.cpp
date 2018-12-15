4267f189c161b9a81854d2efe430fe50014c9753
Maximo Silva <42160673+meisimo@users.noreply.github.com>
2018-12-14 20:40:32 -0500

    Solved 2485
    In the prev version the algorithm that compute the min path of the graph was too slow, because it was an DFS. Then I compute the graph from the original matrix and after that apply a Dijkstra's. The time of the solution was 0.020s

--- URI/prob2485.cpp
+++ URI/prob2485.cpp
@@ -3,100 +3,88 @@
 
 using namespace std;
 
-typedef unsigned unt;
+typedef int unt;
 
-struct crd{
-    unt col, row;
-    crd(){
-        col = row = -1;
-    }
-    crd(unt i, unt j){
-        col = j;
-        row = i;
-    }
-};
-
-struct node{
-    unt w;
-    vector<crd> adjs;
-    node(){
-        w = INF;
-        adjs.clear();
-    }
-};
-
+unt n, m;
 bool Field[100][100];
-node Weights[100][100];
-unt n, m, maxP;
-
-void setW(unt w, crd c){
-    Weights[c.row][c.col].w = w;
-}
-
-
-void addAdjs(crd c){
-    unt iRow =((c.row)?c.row-1:0 ), fRow = ((c.row<n-1)?c.row+1:c.row);
-    unt iCol =((c.col)?c.col-1:0 ), fCol = ((c.col<m-1)?c.col+1:c.col);
-//    cout<<"\nBounds:\n("<<iRow<<","<<iCol<<")"<<"\n("<<fRow<<","<<fCol<<")\n";
-    for(unt i=iRow ; i<=fRow ; i++){
-        for(unt j=iCol; j<=fCol; j++ ){
-            if ( (i==c.row&&j==c.col) || !Field[i][j] || Weights[i][j].w<INF)   continue;
-            Weights[c.row][c.col].adjs.push_back(crd(i,j));
-        }
-    }
-
-}
-
-void maxPath(crd c){
-
-    if( maxP < Weights[c.row][c.col].w )    maxP = Weights[c.row][c.col].w;
-    if (Field[c.row][c.col])       addAdjs(c);
-    Field[c.row][c.col] = 0;
-    unt d = Weights[c.row][c.col].w+1;
-
-    for (crd adj:Weights[c.row][c.col].adjs){
-        if ( d >= Weights[adj.row][adj.col].w )
-            continue;
-        setW(d,adj);
-        maxPath(adj);
-    }
-}
-
+unt Node[100][100];
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    unt T, i, j;
-    crd zero;
+    unt T, i, j, aux, ii, jj;
+    unt zero;
+    unt maxP;
+
+    for (i=0, aux=0; i<100; i++, aux+=100)
+        for( j=0; j<100; j++)
+            Node[i][j] = aux+j;
+
+
     cin>>T;
 
     while(T--){
         maxP = 0;
         cin>>n>>m;
+
+        vector<unt> adjs[10000];
+        unt distance[10000];
+        bool processed[10000];
+        priority_queue<pair<int, unt>> q;
+
         for (i=0; i<n; i++)
             for(j=0; j<m; j++){
                 cin>>Field[i][j];
-                Weights[i][j] = node();
+                distance[Node[i][j]] = INF;
+                adjs[Node[i][j]].clear();
             }
+
+        for (i=0; i<n; i++)
+            for(j=0; j<m; j++){
+                if (Field[i][j]){
+                    processed[Node[i][j]] = false;
+                    for(ii=((i)?i-1:0); ii<=i+1;ii++){
+                        if (n<=ii)  continue;
+                        for (jj=((j)?j-1:0); jj<=j+1;jj++){
+                            if( m<=jj||( ii==i && jj==j ))   continue;
+                            adjs[Node[i][j]].push_back(Node[ii][jj]);
+                        }
+                    }
+                }
+            }
+
+
         cin>>i>>j;
-        zero = crd(i-1, j-1);
 
-        setW(0, zero);
-        maxPath(zero);
+        i--;
+        j--;
+        distance[Node[i][j]] = 0;
+        q.push({0,Node[i][j]});
 
-        cout<<maxP-1<<endl;
+        int a, b;
+        while(!q.empty()){
+            a = q.top().second; q.pop();
+            if (processed[a])   continue;
+            processed[a] = true;
+            for (auto adj:adjs[a]){
+                b = adj;
+                if( distance[a]+1 < distance[b] ){
+                    distance[b] = distance[a]+1;
+                    q.push({-distance[b],b});
+                }
+
+            }
+        }
+
+        for (i = 0; i<n; i++)
+            for(j=0; j<m; j++)
+                if ( Field[i][j] && maxP<distance[Node[i][j]])
+                    maxP = distance[Node[i][j]];
+
+        cout<<maxP<<endl;
     }
 
 
     return 0;
 }
-
-/*
-1
-3 4
-1 0 1 1
-1 0 0 1
-0 1 1 0
-1 1
-*/
