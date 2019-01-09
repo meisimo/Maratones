e59ac27a60805a4ca802dd99147a76b1c312c445
Maximo Silva <42160673+meisimo@users.noreply.github.com>
2019-01-09 17:44:32 -0500

    Add files via upload
    Is essentially the same problem than 1774 with a higher upper bound. This solution is a bit more efficient than the used in 1774 because of the priority queue ( the time performance with the PQ is 0.032s and with the sorted array is 0.040s). The solution using the array is in the commented lines

---
+++ URI/prob2404.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#define MAX 501
+using namespace std;
+
+typedef pair<int,pair<int,int>> pp;
+
+int parent[MAX];
+
+void init(int n){
+    while(n){
+        parent[n] = n;
+        n--;
+    }
+}
+
+int findP(int i){
+    if(parent[i]!=i)
+        parent[i] = findP(parent[i]);
+    return parent[i];
+}
+
+bool unit(int a, int b){
+    a = findP(a);
+    b = findP(b);
+
+    if(a!=b){
+        parent[a] = b;
+        return true;
+    }
+    return false;
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int V, E, a, b, w, N, i, S;
+    pp aux;
+    cin>>V>>E;
+
+    N = E;
+//    pp edges[E];
+    priority_queue<pp> edges;
+    init(V);
+
+    while(E--){
+        cin>>a>>b>>w;
+//        edges[E] = make_pair(w,make_pair(a,b));
+        edges.push(make_pair(-w,make_pair(a,b)));
+    }
+
+//    sort(edges, edges+N);
+
+    i = 0;
+    S = 0;
+    V--;
+    while(V){
+//        aux = edges[i++];
+        aux = edges.top(); edges.pop();
+        if( unit(aux.second.first, aux.second.second) ){
+            V--;
+//            S += aux.first;
+            S -= aux.first;
+        }
+    }
+
+    cout<<S<<endl;
+}
